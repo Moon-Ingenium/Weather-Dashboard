72ef2b71f0441bdaf0cfceb32bb334ee187b02fd
Michelle Moon <robbins.michelle@ymail.com>
2020-10-02 20:09:15 -0500

    button works

--- script.js
+++ script.js
@@ -6,63 +6,61 @@ var searchInput = $("#search-text");
 // local storage
 
 
-function displaySearchCity() {
+// function displaySearchCity() {
     // on click event to get city 
     var btn = $(".btn");
     btn.on("click" , function(event)
     {
         alert("this button works");
-        // var city = $(this).attr("data-city");
-        // $("div>").text(city); 
+        var city = $(this).attr("data-city");
+        $("div>").text(city); 
         
     });
 
+// }
 
 
-
-//     var city = $(this).attr("data-city");
-//     var city ="austin";
-//     var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=36edb26270cfd8ba7f33ada2c6f55cab&units=imperial";
-//     $.ajax({
-//         url: queryUrl,
-//         method: "GET"
-//     }).then(function (response) {
-//         console.log(response);
-
-//         var cityDiv = $(".city");
-//         cityDiv.text(" Weather Details:" + response.main.name);
-//         $(".temp").text("Temprature: " + response.main.temp);
-//         $(".wind").text("Wind Speed: " + response.wind.speed);
-//         $(".uvIndex").text("UV Index: " + response.main.uvindex);
-//         // build a h1, append div, displays city name date and weather icon
-//         // build a p, append div displays temp
+    // var city = $(this).attr("data-city");
+    var city ="austin";
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=36edb26270cfd8ba7f33ada2c6f55cab&units=imperial";
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+         var cityDiv = $(".city");
+        cityDiv.text(" Weather Details:" + response.main.name);
+        $(".temp").text("Temprature: " + response.main.temp);
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".uvIndex").text("UV Index: " + response.main.uvindex);
+        // build a h1, append div, displays city name date and weather icon
+        // build a p, append div displays temp
       
-//         // build a p, append div displays wind speed
+        // build a p, append div displays wind speed
      
-//         // build a p, append div displays uv index
+        // build a p, append div displays uv index
     
-// //     });
+    });
 
 
-// // var fiveDayQueryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=36edb26270cfd8ba7f33ada2c6f55cab&units=imperial";
-// // $.ajax({
-// //     url: fiveDayQueryUrl,
-// //     method: "GET"
-// // }).then(function (response) {
-// //     //    gets 5 day current weather
-// //     var forecastEl = $(".five-day-forecast");
-// //     forecastEl.text(" 5-Day Forecast" );
-// //     var fiveDay = [];
-// //     console.log(response.list);
-// //     for (var i = 0; i < response.list.length; i++) {
-// //         if (i % 8 === 0) {
-// //             fiveDay.push(response.list[i]);
-// //         }
-// //         // create 5 day header
-// //         // display date, weather icon, temp, and humidity per day
-// //     }
+// var fiveDayQueryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=36edb26270cfd8ba7f33ada2c6f55cab&units=imperial";
+// $.ajax({
+//     url: fiveDayQueryUrl,
+//     method: "GET"
+// }).then(function (response) {
+//     //    gets 5 day current weather
+//     var forecastEl = $(".five-day-forecast");
+//     forecastEl.text(" 5-Day Forecast" );
+//     var fiveDay = [];
+//     console.log(response.list);
+//     for (var i = 0; i < response.list.length; i++) {
+//         if (i % 8 === 0) {
+//             fiveDay.push(response.list[i]);
+//         }
+//         // create 5 day header
+//         // display date, weather icon, temp, and humidity per day
+//     }
 
 
-// //     //    display weather icon under weather [0].icon
-// //     // img<src=http://openweathermap.org/img/w/01d.png>
-// // });
+//     //    display weather icon under weather [0].icon
+//     // img<src=http://openweathermap.org/img/w/01d.png>
+// });
